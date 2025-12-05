1e95dfe9ad6fc6cffce68bd854517e2164f307f7
anthropologistbiubiu <1319847957@qq.com>
2025-12-06 03:02:34 +0800

    修改：     excel2proto/loader/main.go 	修改：     python (修改的内容, 未跟踪的内容)

--- excel2proto/loader/main.go
+++ excel2proto/loader/main.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func main() {
+func main1() {
 	raw, err := os.ReadFile("../output/activity_weight.bytes")
 	if err != nil {
 		panic(err)
@@ -19,6 +19,7 @@ func main() {
 	if err := proto.Unmarshal(raw, &list); err != nil {
 		panic(err)
 	}
+	fmt.Println(list.GetItems())
 
 	m := make(map[uint32]uint32)
 	for _, v := range list.Items {
@@ -26,4 +27,31 @@ func main() {
 	}
 
 	fmt.Println("反序列化成功，内容：", m)
+}
+
+var ans = make([][]int, 0)
+
+func subsets(nums []int) [][]int {
+	var curNum = []int{}
+	dfs(0, nums, curNum)
+	return ans
+}
+
+func dfs(cur int, nums []int, curNum []int) {
+
+	ans = append(ans, curNum)
+	//fmt.Println("ans", ans)
+
+	for i := cur; i < len(nums); i++ {
+		//fmt.Println("curNum", curNum)
+		curNum = append(curNum, nums[i])
+		dfs(i+1, nums, curNum)
+		curNum = curNum[:len(curNum)-1]
+	}
+}
+
+func main() {
+	result := subsets([]int{1, 2, 3})
+	fmt.Println("所有子集：", result)
+
 }
